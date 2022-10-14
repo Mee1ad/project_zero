1b97c9aa4b5c663d0c6a1e9be905034ad0b64325
Londonist IT Support <itsupport@londonist.co.uk>
2022-10-14 11:26:14 +0300

    add views

--- map/views.py
+++ map/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""Markers view."""
 
-# Create your views here.
+import json
+
+from django.core.serializers import serialize
+from django.views.generic.base import TemplateView
+
+from .models import Location
+
+
+class MarkersMapView(TemplateView):
+    """Markers map view."""
+
+    template_name = "map.html"
+
+    def get_context_data(self, **kwargs):
+        """Return the view context data."""
+        context = super().get_context_data(**kwargs)
+        context["markers"] = json.loads(serialize("geojson", Location.objects.all()))
+        for obj in context["markers"]['features']:
+            obj['properties']['image'] = "http://localhost:8002/images/" + obj['properties']['image']
+        return context
