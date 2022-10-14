f459f824af6cb21b4c7a2f5baeb2270483cc8d97
Londonist IT Support <itsupport@londonist.co.uk>
2022-10-14 11:23:17 +0300

    fix models

--- map/models.py
+++ map/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.contrib.gis.db import models
+from core.settings import PROJECT_ROOT
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    lat = models.CharField(max_length=255, null=True, blank=True)
+    long = models.CharField(max_length=255, null=True, blank=True)
+    coordinate = models.PointField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
