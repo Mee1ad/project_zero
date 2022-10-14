975d43cb676a52c62875697e50da473df292bd3c
Londonist IT Support <itsupport@londonist.co.uk>
2022-10-14 11:24:38 +0300

    add admin tables

--- map/admin.py
+++ map/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin, register
+from .models import Location, Category
 
-# Register your models here.
+
+@register(Location)
+class LocationAdmin(OSMGeoAdmin):
+    list_display = ('id', 'name', 'coordinate')
+
+
+@register(Category)
+class CategoryAdmin(OSMGeoAdmin):
+    list_display = ('id', 'name')
